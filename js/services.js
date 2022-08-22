89446346d94abe1250b6aed8f9f6ac064081b220
petrceska <ceskape@seznam.cz>
2022-08-22 15:20:00 +0200

    js fix

--- js/services.js
+++ js/services.js
@@ -1,16 +1,25 @@
 function showServices(id) {
-
     var btn = $("#services-" + id);
     var list = $("#services-data-" + id);
     var btn_other = $(".services-button").not("#services-" + id);
     var list_other = $(".services-data").not("#services-data-" + id);
 
-    btn.classList.add("active");
-    btn.classList.remove("inactive");
-    list.style.color = "red";
+    btn.addClass("active");
+    btn.removeClass("inactive");
+    list.addClass("active");
+    list.removeClass("inactive");
 
-    btn_other.each(function (i, el) {
-        el.style.color = "red";
-        // el.style.display = "block";
+    btn_other.each(function () {
+        $(this).addClass("inactive");
+        $(this).removeClass("active");
+    });
+
+    list_other.each(function () {
+        $(this).addClass("inactive");
+        $(this).removeClass("active");
     });
 }
+
+$(document).ready(function () {
+    showServices(1);
+});
